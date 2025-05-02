081afc347ad55c8ac622a46e9fa43042a9d34b51
Tetromiko <deberger.studio@gmail.com>
2025-05-02 17:41:01 +0300

    add relations screens

---
+++ app/(tabs)/profile/(relations)/followers.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+export default function FollowersScreen() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/profile/(relations)/friends.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+export default function FriendsScreen() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});
